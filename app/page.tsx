5a9de203356b9e8d9157c7234fabd25eaf04b8d6
Elihan303 <elihantejada@gmail.com>
2026-03-22 19:31:25 -0400

    feat: about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import { Header } from "@/components/Header";
 import { HeroCarousel } from "@/components/HeroCarousel";
+import { AboutSection } from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">
-     <HeroCarousel />
-            {/*<AboutSection />
-        <ServicesCards />
+      <HeroCarousel />
+      <AboutSection />
+        {/* <ServicesCards />
         <WhyChooseUs />
         <MissionVisionValues />
         <StatsSection />
